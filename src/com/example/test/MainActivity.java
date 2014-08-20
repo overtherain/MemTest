6590451e3a53cff18c39e3106f7d1f02ca18e863
M.R.Z <zgd1348833@gmail.com>
2014-08-20 12:06:04 +0800

    Modify for UI

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -1,66 +1,81 @@
 package com.example.test;
 
+import com.cg.memtest.R;
+import com.jnitest.memutils.memcheck;
 import com.self.debug.Logger;
 
-import android.support.v7.app.ActionBarActivity;
-import android.annotation.SuppressLint;
+//import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.VideoView;
 
-public class MainActivity extends ActionBarActivity {
+//public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 	private final static String TAG = "MainActivity";
 	private final static String VIDEO_PATH = "/storage/sdcard0/test.avi";
-	private TextView tv;
+	private TextView resultTv;
+	private EditText etNum;
 	private Button startBtn;
 	private Button stopBtn;
-	private ImageView img;
 	private VideoView vplayer;
+	private int result = 0;
+	private int thread = 0;
+	private Thread tdMemcheck;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Logger.d(TAG, "onCreate.");
-		tv = (TextView) findViewById(R.id.tips);
-		tv.setText("FAIL");
-		tv.setTextSize(120);
-		tv.setTextColor(android.graphics.Color.WHITE);
-		tv.setShadowLayer(1.0f, 2.0f, 2.0f, android.graphics.Color.BLACK);
-		tv.setVisibility(View.VISIBLE);
-		startBtn = (Button) findViewById(R.id.start);
+	public void initview() {
+		resultTv = (TextView) findViewById(R.id.tips);
+		resultTv.setText("FAIL");
+		resultTv.setTextSize(120);
+		resultTv.setTextColor(android.graphics.Color.RED);
+		resultTv.setShadowLayer(1.0f, 2.0f, 2.0f, android.graphics.Color.BLUE);
+		resultTv.setVisibility(View.VISIBLE);
+		etNum = (EditText) findViewById(R.id.num);
+		startBtn = (Button) findViewById(R.id.startBtn);
 		startBtn.setText("START");
 		startBtn.setTextColor(android.graphics.Color.GREEN);
 		startBtn.setShadowLayer(1.0f, 2.0f, 2.0f, android.graphics.Color.BLACK);
 		startBtn.setVisibility(View.VISIBLE);
-		startBtn.setOnClickListener(new OnClickListener(){
+		startBtn.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-//				vplayer.get
 				vplayer.setVideoPath(VIDEO_PATH);
 				vplayer.start();
 				startBtn.setVisibility(View.INVISIBLE);
 				stopBtn.setVisibility(View.VISIBLE);
+				resultTv.setVisibility(View.INVISIBLE);
+				thread = Integer.parseInt(etNum.getText().toString());
+				// result = new memutils(thread).doTask();
+				for (int i = 0; i < thread; i++) {
+					tdMemcheck = new Thread(new Runnable() {
+
+						@Override
+						public void run() {
+							// TODO Auto-generated method stub
+							int no = Integer.parseInt(Thread.currentThread().getName());
+							String tmp = new memcheck().doTask(no);
+							android.util.Log.d(TAG, "tmp: " + tmp);
+						}
+					});
+					tdMemcheck.setName("" + i);
+					tdMemcheck.start();
+				}
 			}
 		});
-		stopBtn = (Button)findViewById(R.id.stop);
+		stopBtn = (Button) findViewById(R.id.stopBtn);
 		stopBtn.setText("STOP");
 		stopBtn.setTextColor(android.graphics.Color.GREEN);
 		stopBtn.setShadowLayer(1.0f, 2.0f, 2.0f, android.graphics.Color.BLACK);
 		stopBtn.setVisibility(View.INVISIBLE);
-		stopBtn.setOnClickListener(new OnClickListener(){
+		stopBtn.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
@@ -68,15 +83,18 @@ public class MainActivity extends ActionBarActivity {
 				vplayer.pause();
 				startBtn.setVisibility(View.VISIBLE);
 				stopBtn.setVisibility(View.INVISIBLE);
+				resultTv.setVisibility(View.VISIBLE);
+				// tdMemcheck.destroy();
+				// tdMemcheck.dumpStack();
 			}
 
 		});
-		vplayer = (VideoView)findViewById(R.id.player);
+		vplayer = (VideoView) findViewById(R.id.player);
 		vplayer.setKeepScreenOn(true);
 		vplayer.setVisibility(View.VISIBLE);
 		vplayer.setMinimumHeight(750);
 		vplayer.setMinimumWidth(450);
-		vplayer.setOnCompletionListener(new OnCompletionListener(){
+		vplayer.setOnCompletionListener(new OnCompletionListener() {
 
 			@Override
 			public void onCompletion(MediaPlayer arg0) {
@@ -85,8 +103,16 @@ public class MainActivity extends ActionBarActivity {
 				vplayer.setVideoPath(VIDEO_PATH);
 				vplayer.start();
 			}
-			
+
 		});
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Logger.d(TAG, "onCreate.");
+		initview();
 	}
 
 	@Override
@@ -94,30 +120,5 @@ public class MainActivity extends ActionBarActivity {
 		// TODO Auto-generated method stub
 		super.onResume();
 		vplayer.resume();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		// TODO Auto-generated method stub
-		return super.onTouchEvent(event);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
