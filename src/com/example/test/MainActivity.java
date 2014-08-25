59aecf6fa0a97f5b85bb1fbd81c6bfada2e80de1
M.R.Z <zgd1348833@gmail.com>
2014-08-25 21:09:44 +0800

    update for 1 thread test

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -4,7 +4,6 @@ import com.cg.memtest.R;
 import com.jnitest.memutils.memcheck;
 import com.self.debug.Logger;
 
-//import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
@@ -13,7 +12,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
